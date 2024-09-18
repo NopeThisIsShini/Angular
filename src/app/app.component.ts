221754e01a72a59309d8daded9b5236fcbcda5ef
NopeThisIsShini <getin2santa@gmail.com>
2024-09-18 14:09:23 +0530

    local storage service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { AfterViewInit, Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { RoomsComponent } from './components/rooms/rooms.component';
+import { localStorageToken } from './localStorage.token';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements OnInit,AfterViewInit{
   title = 'angular';
+  constructor(
+    @Inject(localStorageToken) private localStorage: Storage
+  ){
 
+  }
+  ngOnInit(): void {
+    // this is how to set structured data in local storage
+    this.localStorage.setItem('token', 'testjwttoken');
+  }
   // dynamic component loading 
   @ViewChild('room', {read: ViewContainerRef}) vcr!: ViewContainerRef;
   ngAfterViewInit(): void {
---
+++ src/app/localStorage.token.ts
@@ -0,0 +1,6 @@
+import { InjectionToken } from "@angular/core";
+
+export const localStorageToken = new InjectionToken<Storage>('localStorage', {
+    providedIn: 'root',
+    factory: () => localStorage
+})
