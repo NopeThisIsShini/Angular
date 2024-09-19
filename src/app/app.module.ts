e5afd539a55dfbabccd4464cab58288c17d19018
NopeThisIsShini <getin2santa@gmail.com>
2024-09-19 11:36:07 +0530

    APP_INITIALIZER

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +12,12 @@ import { EmployeeComponent } from './components/employee/employee.component';
 import { APP_CONFIG, APP_SERVICE_CONFIG } from './AppConfig/appconfig.service';
 import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { requestInterceptor } from './interceptor/request.interceptor';
+import { InitService } from './components/rooms/services/init.service';
+
+// APP_INITIALIZER 
+function initializeAppFactory(initService: InitService) {
+  return () => initService.init();
+ }
 
 @NgModule({
   declarations: [
@@ -32,6 +38,12 @@ import { requestInterceptor } from './interceptor/request.interceptor';
     {
       provide: APP_SERVICE_CONFIG,
       useValue: APP_CONFIG,
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeAppFactory,
+      deps: [InitService],
+      multi: true
     },
     provideHttpClient(
       withInterceptors([requestInterceptor])
